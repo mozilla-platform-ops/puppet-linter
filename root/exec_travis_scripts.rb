9f933e56e919098439a34be1c48b719bf862b42c
Jake Watkins <dividehex@gmail.com>
2018-04-04 11:02:58 -0700

    Fix system exit value reference

--- root/exec_travis_scripts.rb
+++ root/exec_travis_scripts.rb
@@ -5,6 +5,6 @@ travis_yml = YAML.load_file('.travis.yml')
 travis_yml["script"].each do |x|
   puts "Executing: #{x}"
   system(x)
-  puts "Command exit code #{$?.exitcode}"
+  puts "Command exit code #{$?.exitstatus}"
 end
 
